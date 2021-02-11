875e6ad6c6d4f72d08504534e2df58997e91cde6
Gary Vladimir Núñez <gary@garybricks.com>
2021-02-10 19:59:14 -0600

    Add files via upload

--- src/server/index.js
+++ src/server/index.js
@@ -1,19 +1,13 @@
 const dotenv = require('dotenv');
-dotenv.config();
 var path = require('path');
 const express = require('express');
 const mockAPIResponse = require('./mockAPI.js');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
-var json = {
-    title: 'test json response',
-    message: 'this is a message',
-    time: 'now',
-};
+dotenv.config();
 
-
-var KEY = process.env.API_KEY,
+const API_KEY = process.env.API_KEY;
 
 const app = express();
 app.use(cors());
@@ -41,5 +35,4 @@ app.get('/test', function (req, res) {
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
     console.log('Example app listening on port 8081!');
-    console.log(KEY);
 });
